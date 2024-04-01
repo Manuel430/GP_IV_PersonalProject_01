c14b670524ede1ac43c3a6dd582781ee2d7f9764
Manuel Rodriguez <mgrodri5@student.uiwtx.edu>
2024-03-31 23:09:39 -0500

    Game Complete, need to package

---
+++ Source/GPIV_Project_01/Private/Widget/GPIV_MainMenuWidget.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widget/GPIV_MainMenuWidget.h"
+
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
+
+void UGPIV_MainMenuWidget::NativeConstruct()
+{
+	if (PlayButton)
+	{
+		PlayButton->OnClicked.AddDynamic(this, &UGPIV_MainMenuWidget::OnPlayClick);
+	}
+
+	if (QuitButton)
+	{
+		QuitButton->OnClicked.AddDynamic(this, &UGPIV_MainMenuWidget::OnQuitClick);
+	}
+}
+
+void UGPIV_MainMenuWidget::OnPlayClick()
+{
+	UWorld* World = GetWorld();
+
+	if (World)
+	{
+		UGameplayStatics::OpenLevel(World, TEXT("GameMap_01"));
+	}
+}
+
+void UGPIV_MainMenuWidget::OnQuitClick()
+{
+	APlayerController* SpecificPlayer = GetWorld()->GetFirstPlayerController();
+	UKismetSystemLibrary::QuitGame(GetWorld(), SpecificPlayer, EQuitPreference::Quit, true);
+}
